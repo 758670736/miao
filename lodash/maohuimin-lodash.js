b813e3c01efd67c104bb9a006e0252c482cc2d92
maohuimin <758670736@qq.com>
2022-09-15 10:12:46 +0800

    lodash

--- lodash/maohuimin-lodash.js
+++ lodash/maohuimin-lodash.js
@@ -1,1 +1,17 @@
-var maohuimin = []
+var maohuimin = {
+  add:
+    function add(a, b) {
+      return a + b
+    },
+  compact:
+    function compact(ary) {
+      var result = []
+      for (var i = 0; i < ary.length; i++) {
+        if (ary[i]) {
+          result.push(ary[i])
+        }
+      }
+      return result
+    },
+  
+}
