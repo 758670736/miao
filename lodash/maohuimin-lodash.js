ccae0aeca0d3e667a03f864783f3dbdc05d100e0
maohuimin <758670736@qq.com>
2022-09-15 10:53:53 +0800

    fill

--- lodash/maohuimin-lodash.js
+++ lodash/maohuimin-lodash.js
@@ -3,15 +3,42 @@ var maohuimin = {
     function add(a, b) {
       return a + b
     },
-  compact:
-    function compact(ary) {
+  chunk:
+    function chunk(array, nums) {
       var result = []
-      for (var i = 0; i < ary.length; i++) {
-        if (ary[i]) {
-          result.push(ary[i])
+    },
+  compact:
+    function compact(array) {
+      var result = []
+      for (var i = 0; i < array.length; i++) {
+        if (array[i]) {
+          result.push(array[i])
         }
       }
       return result
     },
-  
+  difference:
+    function difference(array, val) {
+
+    },
+  drop:
+    function drop(array, n = 1) {
+      if (n > array.length) {
+        return []
+      }
+      var result = []
+      for (var i = 0; i < array.length; i++) {
+        if (i >= n) {
+          result.push(array[i])
+        }
+      }
+      return result
+    },
+  fill:
+    function fill(array, value, start = 0, end = array.length) {
+      for (var i = start; i < end; i++) {
+        array[i] = value
+      }
+      return array
+    }
 }
